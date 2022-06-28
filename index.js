5912b944eabc9e00e3ef40741d39373c40c09329
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-28 15:17:08 +0200

    ex 6

--- index.js
+++ index.js
@@ -85,3 +85,39 @@
 // })
 
 // console.log(soldOut)
+
+// 06
+// const cakes = [
+// 	{
+// 		name: "cake",
+// 		flavor: "vanilla",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "brownie",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "pie",
+// 		flavor: "strawberry",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "muffin",
+// 		flavor: "pistachio",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "donut",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// ]
+
+// const pie = cakes.find((cake) => {
+//     return cake.name === "pie"
+// })
+
+// console.log(pie)
+
