ff586082ea9c25c325ae37ec0cc10c6367161259
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-28 14:06:56 +0200

    2 ex

---
+++ index.js
@@ -0,0 +1,28 @@
+// 01
+// const array = [1,2,3,4,5]
+
+// const double = array.map((number) => {
+//     return number * 2
+// })
+
+// console.log(double)
+
+// 02
+const longNames = [
+    {
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+]
+
+const shortNames = longNames.map((name) =>{
+    return({
+        name: `${name.firstName} ${name.lastName}`
+    })
+})
+
+console.log(shortNames)
