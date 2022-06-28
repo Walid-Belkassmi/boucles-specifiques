5b84502b8acb05a8974bdb5b1022c38721405af7
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-28 14:23:55 +0200

    3e ex

--- index.js
+++ index.js
@@ -8,21 +8,30 @@
 // console.log(double)
 
 // 02
-const longNames = [
-    {
-        firstName: "Jane",
-        lastName: "Doe"
-    },
-    {
-        firstName: "John",
-        lastName: "Smith"
-    }
-]
+// const longNames = [
+//     {
+//         firstName: "Jane",
+//         lastName: "Doe"
+//     },
+//     {
+//         firstName: "John",
+//         lastName: "Smith"
+//     }
+// ]
 
-const shortNames = longNames.map((name) =>{
-    return({
-        name: `${name.firstName} ${name.lastName}`
-    })
-})
+// const shortNames = longNames.map((name) =>{
+//     return({
+//         name: `${name.firstName} ${name.lastName}`
+//     })
+// })
 
-console.log(shortNames)
+// console.log(shortNames)
+
+// 03
+// const array = [1, "toto", 34, "JavaScript", 8]
+
+// const numbers = array.filter((number) => {
+//     return(number > 0)
+// })
+
+// console.log(numbers)
