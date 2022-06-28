c06785bd2797d9dfadbdbcc9e8d8ca01b3bce0f8
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-28 14:44:10 +0200

    ex 4

--- index.js
+++ index.js
@@ -35,3 +35,12 @@
 // })
 
 // console.log(numbers)
+
+// 04
+// const numbers = [1,2,3,4,5,6,7,8]
+
+// const even = numbers.filter((number) =>{
+//     return(number % 2 === 0)
+// })
+
+// console.log(even)
